016c46ec4dad547d6c0b2a577b5bfca97a8c6d57
Farhan Khan <farhan01617@gmail.com>
2019-02-13 12:33:20 +0600

    updatre

---
+++ thegncy-element.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Plugin Name: TheGncy Element
+* Plugin URI: https://github.com/thegncy/thegncy-plugin
+* Description: After install the thegncy WordPress Theme, you must need to install this "TheGncy Element" first to get all functions of thegncy WP Theme.
+* Version: 1.0.0
+* Author: BusinessTheme
+* Author URI: http://themeforest.net/user/business-theme
+* Text Domain: thegncy
+* License: GPL/GNU.
+*/
+
+
+/**----------------------------------------------------------------*/
+/* Include all file
+/*-----------------------------------------------------------------*/  
+
+include_once(dirname( __FILE__ ). '/inc/custom-posts.php');
+include_once(dirname( __FILE__ ). '/inc/elementor/elementor.php');
+include_once(dirname( __FILE__ ). '/inc/recent-post.php');
